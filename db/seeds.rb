a9141a5a0a820dfb55cb67033370a88107a8e52e
devstar625 <devstar625@gmail.com>
2021-05-14 20:25:23 +0300

    rubyproject_one
    this is my first ruby project

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Post.create(title: 'Hello World 1', content: 'Test 1')   
+Post.create(title: 'Hello World 2', content: 'Test 2')  
+Post.create(title: 'Hello World 3', content: 'Test 3')  
+Post.create(title: 'Hello World 4', content: 'Test 4')
